e4df1d3f87dd4e71bc858d5f03018599edfe0818
Shashank Shekhar <79270265+stunysteww@users.noreply.github.com>
2021-09-12 11:52:49 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+if("servicWorker" in Navigator){
+navigator.serviceWorker.register("sw.js").then(registration => {
+    console.log("SW Registered!");
+    console.log(registration);
+
+}).catch(error =>{
+
+    console.log("SW Registeration Failed!");
+    console.log(error);
+
+});
+}
---
+++ sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open("static").then(cache =>{
+            return cache.addAll(["./","./src/master.css","./images/logo192.png"]);
+        })
+    );
+});
+self.addEventListener("fetch",e =>{
+    console.log('Intercepting fetch request for: ${e.request.url}');
+});
